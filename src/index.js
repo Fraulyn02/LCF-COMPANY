2332981027182788822e801e080700566bce736c
Angelo Paredes Dominguez <angeloJS04>
2019-12-23 16:01:07 -0400

    conexion del formulario con el servidor

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 
+
 //settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+
 //middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //router
 app.use(require('./routes/index'));
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 //listening the server
 app.listen(app.get('port'), () => { console.log(`server on port 3000`)} );
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,8 +7,14 @@ router.get('/', (req, res) => {
     res.render('index.html', {title: 'LCF COMPANY'})
 });
 
-router.get('/contact', (req, res) => {
+router.get('/contact', (req, res, next) => {
     res.render('contact.html', {title: 'LCF CONTACTS'})
 });
+router.post('/contact', (req, res, next) => {
+    console.log(req.body);
+    res.send('Hola')
+    
+   
+})
 
 module.exports = router;
