73097f7a0ad6c826532557ae6ef7471512994fad
angeloJS04 <52179030+angeloJS04@users.noreply.github.com>
2020-01-10 15:37:34 -0400

    validate form

--- src/public/js/contact.js
+++ src/public/js/contact.js
@@ -1,15 +1,8 @@
 alert('Hola');
+console.log('sfsfsf')
 const email = document.getElementById('email');
 const message = document.getElementById('mensaje');
 const name = document.getElementById('name');
 const phone = document.getElementById('phone');
 const btnSend = document.getElementById('btnSend');
 
-
-function eventListener(){
-    document.addEventListener('DOMContentLoaded', inicioApp)
-} eventListener();
-
-function inicioApp(){
-    btnSend.disabled = true;
-}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
- const { Router } = require ('express');
- const router = Router();
+const { Router } = require ('express');
+const router = Router();
 const nodemailer = require('nodemailer');
 require ('dotenv').config();
 
